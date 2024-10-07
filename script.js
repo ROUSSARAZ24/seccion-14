bd795db72af59d91bd862a6efabd555dddaf5fc0
ROUSSARAZ24 <frazuajev@gmail.com>
2024-10-07 16:58:11 -0500

    punto

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded",function(){
 const button = document.getElementById("changeTextButton");
 button.addEventListener("click",function(){
 const introSection=document.getElementById("intro");
-introSection.innerHTML=<P>El texto ha cambiado despues de hacer click en el boton!!</P>
+introSection.innerHTML=<P>El texto ha cambiado despues de hacer click en el boton!!</P>;
 });
 });
