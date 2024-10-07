e1860f563bae92c54c517cab8a34867510fa058e
ROUSSARAZ24 <frazuajev@gmail.com>
2024-10-07 17:15:54 -0500

    comilla simple

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-document.addEventListener("DOMContentLoaded",function(){
-const button = document.getElementById("changeTextButton");
-button.addEventListener("click",function(){
-const introSection=document.getElementById("intro");
-introSection.innerHTML="<P>El texto ha cambiado despues de hacer click en el boton!!</P>";
+document.addEventListener('DOMContentLoaded',function(){
+const button = document.getElementById('changeTextButton');
+button.addEventListener('click',function(){
+const introSection=document.getElementById('intro');
+introSection.innerHTML='<P>El texto ha cambiado despues de hacer click en el boton!!</P>';
 });
 });
