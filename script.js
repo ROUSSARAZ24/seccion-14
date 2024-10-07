3ca5dd684eefd090bdf114a62f1dfa6cb1d71261
ROUSSARAZ24 <frazuajev@gmail.com>
2024-10-07 16:39:32 -0500

    comill

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener("DOMContentLoaded",function(){
 const button = document.getElementById("changeTextButton");
-button.addEventListener(click,function(){
+button.addEventListener("click",function(){
 const introSection=document.getElementById("intro");
 introSection.innerHTML=<P>El texto ha cambiado despues de hacer click en el boton!!</P>
 });
